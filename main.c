ca0c841e1a5a5b3f53d03a4b327fb4dd814524fe
chickazee4 <lyndsay@vivaldi.net>
2022-09-26 11:28:22 -0600

    fix text handling

--- main.c
+++ main.c
@@ -12,6 +12,21 @@ void on_ready(struct discord *client, const struct discord_ready *event) {
     log_info("Logged in as %s!", event->user->username);
 }
 
+char *strip_url(char *orig)
+{
+    int len = strlen(orig);
+    char *new = malloc(strlen(orig) * sizeof(char));
+    int j = 0;
+    for(int i = 0; i < len; i++){
+        if (orig[i] != '\\' && orig[i] != '\"'){
+            new[j] = orig[i];
+            j++;
+        }
+    }
+    new[j] = '\0';
+    return new;
+}
+
 // remove extraneous whitespace
 void strip(char *orig, int len)
 {
@@ -22,10 +37,44 @@ void strip(char *orig, int len)
     }
 }
 
-void split_text(char *orig, char *buf1, char *buf2)
+void split_text(char *orig, char **buf1, char **buf2)
 {
     int maxlen = strlen(orig);
     strip(orig, maxlen);
+    char *token;
+    char *out = strdup(orig);
+    int wc = 0;
+    // pass 1
+    while ((token = strsep(&out, " "))){
+        wc++;
+    }
+    free(out);
+    if (wc <= 1){
+        *buf1 = orig;
+        *buf2 = " ";
+    } else {
+        *buf1 = malloc(maxlen);
+        *buf1[0] = '\0';
+        *buf2 = malloc(maxlen);
+        *buf2[0]= '\0';
+        char *out2 = strdup(orig);
+        int i = 0;
+        // pass 2
+        while ((token = strsep(&out2, " "))){
+            if (i < wc / 2){
+                strcat(*buf1, token);
+                strcat(*buf1, " ");
+            } else {
+                strcat(*buf2, token);
+                strcat(*buf2, " ");
+            }
+            i++;
+        }
+        int len1 = strlen(*buf1);
+        int len2 = strlen(*buf2);
+        free(out2);
+    }
+    /*
     int *divider_indices = malloc(sizeof(int));
     int indexct = 0;
 
@@ -47,51 +96,57 @@ void split_text(char *orig, char *buf1, char *buf2)
                 break;
         }
     }
+    if (indexct <= 1) {
+        buf1 = orig;
+        buf2 = "";
+    } else {
+        int middle = maxlen / 2;
+        int lowest = 0, distance;
+        // find the index closest to the middle of the post
+        for(int j = 0; j++; j < indexct){
+            distance = abs(middle - divider_indices[j]);
+            if(distance < lowest)
+                lowest = divider_indices[j];
+        }
 
-    int middle = maxlen / 2;
-    int lowest = 0, distance;
-    // find the index closest to the middle of the post
-    for(int j = 0; j++; j < indexct){
-        distance = abs(middle - divider_indices[j]);
-        if(distance < lowest)
-            lowest = divider_indices[j];
+        buf1 = malloc(lowest + 1);
+        buf2 = malloc(maxlen - lowest - 2);
+        strncpy(buf1, orig, lowest + 1);
+        strncpy(buf2, orig + lowest + 2, maxlen - lowest - 2);
     }
-
-    buf1 = malloc(lowest + 1);
-    buf2 = malloc(maxlen - lowest - 2);
-    strncpy(buf1, orig, lowest + 1);
-    strncpy(buf2, orig + lowest + 2, maxlen - lowest - 2);
+    */
 }
 
 char *create_meme(char *text) {
     char *buf1;
     char *buf2;
-    split_text(text, buf1, buf2);
+    split_text(text, &buf1, &buf2);
     Imgflip_request im = create_random_request(buf1, buf2);
     Imgflip_response ir = request_meme(&im);
     if (ir.success == 0){
         perror(ir.error_message);
         exit(1);
     } else {
-        return ir.url;
+        char *stripped_url = strip_url(ir.url);
+        printf("%s\n", stripped_url);
+        return stripped_url;
     }
 }
 
 void on_message(struct discord *client, const struct discord_message *event) {
+    printf("Message read with contents %s from author id %lu\n", event->content, event->author->id);
     if (event->author->id == TARGET_USER_ID){
         srand(time(0));
         if ((rand() % CHANCE_DENOMINATOR) == 0) {
-            if (event->attachments == NULL && event->embeds == NULL){
                 char *url = create_meme(event->content);
                 struct discord_create_message params = { .content = url };
                 discord_create_message(client, event->channel_id, &params, NULL);
-            }
         }
     }
 }
 
 int main(void) {
-    printf("Please enter Imgflip password: \n");
+    printf("Please enter Imgflip password: ");
 
     imgflip_password = malloc(256);
     scanf("%s", imgflip_password);
